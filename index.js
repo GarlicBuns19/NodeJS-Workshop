c6ea7f4b27fda6c6432651fe8cbce67f8a425e8b
GarlicBuns19 <planterx26@gmail.com>
2022-07-26 14:32:29 +0200

    watching vids

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-test = require('./test')
+const file = require('fs');
+const path = require('path');
 
-console.log(test.thing('hello'))
+file.readFile('./data.txt','utf8',(err,data) => {
+    if(err)throw err;
+    console.log(data)
+})
+
+file.witeFile(path.join(__dirname,'./data.txt'),
+    'Example 3', err=> {
+    
+    }
+)
